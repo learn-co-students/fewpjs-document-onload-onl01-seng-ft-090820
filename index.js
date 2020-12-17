50fceea2488c057d77b3e3b22818042c860c63e3
adarvin14 <amdarvin14@gmail.com>
2020-12-17 14:44:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    const a = document.querySelector("#text")
+    a.innerHTML = "This is really cool!"
+  });
